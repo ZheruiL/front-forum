74ebf01a7a2afc35fc1133ddb9f7881163c8d879
sian <diana643643@gmail.com>
2019-11-20 23:24:19 +0100

    finished part of add topic page

--- src/component/topic/AddTopicComponent.js
+++ src/component/topic/AddTopicComponent.js
@@ -1,20 +1,82 @@
 import React, { Component } from 'react'
 import Template from "../template/template"
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import ReactDOM from 'react-dom';
+import {Editor, EditorState} from 'draft-js';
+
 // import ApiService from "../../service/ApiService";
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 500,
+  },
+}));
+
+const styles = {
+  editor: {
+    border: '1px solid gray',
+    minHeight: '6em'
+  }
+};
+
+function AddTopicForm(props){
+  const classes = useStyles()
+  return (
+    <Paper className={classes.root}>
+  <form>
+    <div>
+    <TextField
+          id="outlined-textarea"
+          label="Title"
+          multiline
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+        />
+    </div>
+    {/*<div style={styles.editor} onClick={this.focusEditor}> 
+        <Editor
+          ref={this.setEditor}
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+        />
+  </div>*/}
+  </form>
+</Paper>
+  )
+}
 
 class AddTopicComponent extends Component{
 
   constructor(props){
     super(props);
     this.state ={
-      _id: '',
+     /* _id: '',
       title: '',
-      content: '',
-      /*price: '',
-      rating: '',
-      available: '',*/
+      content: '',*/
+      editorState: EditorState.createEmpty(),
     }
     this.saveTopic = this.saveTopic.bind(this);
+    this.onChange = (editorState) => this.setState({editorState});
+    this.setEditor = (editor) => {
+      this.editor = editor;
+    };
+    this.focusEditor = () => {
+      if (this.editor) {
+        this.editor.focus();
+      }
+    };
+  }
+
+  componentDidMount() {
+    this.focusEditor();
   }
 
   saveTopic = (e) => {
@@ -34,13 +96,7 @@ class AddTopicComponent extends Component{
     return(
       <div>
         <Template>
-          <form>
-            <label>
-              名字:
-              <input type="text" name="name" />
-            </label>
-            <input type="submit" value="提交" />
-          </form>
+          <AddTopicForm />
         </Template>
       </div>
     );
