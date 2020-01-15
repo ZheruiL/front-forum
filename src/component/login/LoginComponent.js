fed885c8686198c258ed73d3a165c56ddc55e0b9
liujianyi622 <47506819+liujianyi622@users.noreply.github.com>
2020-01-15 10:35:26 +0100

    Add files via upload

--- src/component/login/LoginComponent.js
+++ src/component/login/LoginComponent.js
@@ -12,100 +12,113 @@ import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
+import Template from '../template/Template'
+import { FPaper } from '../template/weight/surface/FPaper'
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
-  }
-}))
-
-function Flogin (props) {
-  const classes = useStyles()
-
-  return (
-    <Container component='main' maxWidth='xs'>
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component='h1' variant='h5'>
-          Login
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
-            id='email'
-            label='Email Address'
-            name='email'
-            autoComplete='email'
-            autoFocus
-          />
-          <TextField
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
-            name='password'
-            label='Password'
-            type='password'
-            id='password'
-            autoComplete='current-password'
-          />
-          <FormControlLabel
-            control={<Checkbox value='remember' color='primary' />}
-            label='Remember me'
-          />
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            color='primary'
-            className={classes.submit}
-          >
-            Login
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link to='forgetPassword' variant='body2'>
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link to='/signup' variant='body2'>
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8} />
-    </Container>
-  )
+const paperStyle = {
+  marginTop: 10,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center'
 }
+
+const avatarStyle = {
+  margin: 1,
+  backgroundColor: 'red'
+}
+
+const formStyle = {
+  width: '100%', // Fix IE 11 issue.
+  marginTop: 1
+}
+
+const submitStyle = {
+  margin: 3
+}
+
 export class LoginComponent extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+      user: [],
+      message: null
+    }
+  }
+
+  componentDidMount () {
+    this.setState({ isLoading: false })
+  }
+
   render () {
     return (
-      <Flogin />
+      <Template isLoading={this.state.isLoading}>
+        <FPaper>
+          <Container component='main' maxWidth='xs'>
+            <CssBaseline />
+            <div style={paperStyle}>
+              <Avatar style={avatarStyle}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component='h1' variant='h5'>
+                Login
+              </Typography>
+              <form style={formStyle} noValidate>
+                <TextField
+                  variant='outlined'
+                  margin='normal'
+                  required
+                  fullWidth
+                  id='email'
+                  label='Email Address'
+                  name='email'
+                  autoComplete='email'
+                  autoFocus
+                />
+                <TextField
+                  variant='outlined'
+                  margin='normal'
+                  required
+                  fullWidth
+                  name='password'
+                  label='Password'
+                  type='password'
+                  id='password'
+                  autoComplete='current-password'
+                />
+                <FormControlLabel
+                  control={<Checkbox value='remember' color='primary' />}
+                  label='Remember me'
+                />
+                <Button
+                  type='submit'
+                  fullWidth
+                  variant='contained'
+                  color='primary'
+                  style={submitStyle}
+                >
+                  Login
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link to='forgetPassword' variant='body2'>
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link to='/signup' variant='body2'>
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </form>
+            </div>
+            <Box mt={8} />
+          </Container>
+        </FPaper>
+      </Template>
     )
   }
 }
