3c233b5a6c7306f81c2d5220ee66f327ed1a3d90
zherui.li@outlook.com <zherui.li@outlook.com>
2020-01-16 19:34:34 +0100

    login model

--- src/component/login/SignupComponent.js
+++ src/component/login/SignupComponent.js
@@ -13,9 +13,12 @@ import Box from '@material-ui/core/Box'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
+import Template from '../template/Template'
+import { FPaper } from '../template/weight/surface/FPaper'
+import Swal, { isLoading } from 'sweetalert2'
+import ApiUser from "../../api/ApiUser"
 
 const paperStyle = {
-  marginTop: '20%',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center'
@@ -32,95 +35,160 @@ const submitStyle = {
   marginTop: '5%',
   marginBottom: '5%'
 }
-export class SignupComponent extends React.Component {
+export class SignUpComponent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.state = {
+      isLoading: true,
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      message: null
+    }
+  }
+
+  componentDidMount () {
+    this.setState({ isLoading: false })
+  }
+
+  handleChange (event, name) {
+    this.setState({ [name]: event.target.value })
+    // console.log(this.state)
+  }
+
+  handleSubmit (event) {
+    let error = ''
+    if(this.state.password !== this.state.passwordConfirm){
+      error += 'Two input password must be consistent<br/>'
+    }
+    const user = { username: this.state.username, email: this.state.email, password: this.state.password }
+    // console.log(topic)
+    if (user.username === '') {
+      error += 'username is required<br/>'
+    }
+    if (user.email === '') {
+      error += 'email is required<br/>'
+    }
+    if (user.password === '') {
+      error += 'password is required<br/>'
+    }
+    if (error !== '') {
+      Swal.fire({
+        title: 'Error!',
+        html: error,
+        icon: 'error',
+        confirmButtonText: 'OK'
+      })
+      return
+    }
+    this.setState({ isLoading: true })
+    ApiUser.register(user)
+      .then(res => {
+        console.log(res);
+        this.props.history.push('/')
+      })
+  }
+
   render () {
     return (
-      <Container component='main' maxWidth='xs'>
-        <CssBaseline />
-        <div style={paperStyle}>
-          <Avatar style={avatarStyle}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component='h1' variant='h5'>
-            Sign up
-          </Typography>
-          <form style={formStyle} noValidate>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  autoComplete='fname'
-                  name='userName'
-                  variant='outlined'
-                  required
+      <Template isLoading={this.state.isLoading}>
+        <FPaper>
+          <Container component='main' maxWidth='xs'>
+            <CssBaseline />
+            <div style={paperStyle}>
+              <Avatar style={avatarStyle}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component='h1' variant='h5'>
+                Sign up
+              </Typography>
+              <form style={formStyle} noValidate>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      onChange={(e) => this.handleChange(e, 'username')}
+                      autoComplete='username'
+                      name='userName'
+                      variant='outlined'
+                      required
+                      fullWidth
+                      id='userName'
+                      label='User Name'
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      onChange={(e) => this.handleChange(e, 'email')}
+                      variant='outlined'
+                      required
+                      fullWidth
+                      id='email'
+                      label='Email Address'
+                      name='email'
+                      autoComplete='email'
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      onChange={(e) => this.handleChange(e, 'password')}
+                      variant='outlined'
+                      required
+                      fullWidth
+                      name='password'
+                      label='Password'
+                      type='password'
+                      id='password'
+                      autoComplete='current-password'
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      onChange={(e) => this.handleChange(e, 'passwordConfirm')}
+                      variant='outlined'
+                      required
+                      fullWidth
+                      name='passwordConfirm'
+                      label='Confirm password'
+                      type='password'
+                      id='passwordConfirm'
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControlLabel
+                      control={<Checkbox value='allowExtraEmails' color='primary' />}
+                      label='I want to receive inspiration, marketing promotions and updates via email.'
+                    />
+                  </Grid>
+                </Grid>
+                <Button
+                  type='button'
                   fullWidth
-                  id='userName'
-                  label='User Name'
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant='outlined'
-                  required
-                  fullWidth
-                  id='email'
-                  label='Email Address'
-                  name='email'
-                  autoComplete='email'
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant='outlined'
-                  required
-                  fullWidth
-                  name='password'
-                  label='Password'
-                  type='password'
-                  id='password'
-                  autoComplete='current-password'
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant='outlined'
-                  required
-                  fullWidth
-                  name='passwordConfirm'
-                  label='Confirm password'
-                  type='password'
-                  id='passwordConfirm'
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value='allowExtraEmails' color='primary' />}
-                  label='I want to receive inspiration, marketing promotions and updates via email.'
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type='submit'
-              fullWidth
-              variant='contained'
-              color='primary'
-              style={submitStyle}
-            >
-              Sign Up
-            </Button>
-            <Grid container justify='flex-end'>
-              <Grid item>
-                <Link to='/login' variant='body2'>
+                  variant='contained'
+                  color='primary'
+                  style={submitStyle}
+                  onClick={this.handleSubmit}
+                >
+                  Sign Up
+                </Button>
+                <Grid container justify='flex-end'>
+                  <Grid item>
+                    <Link to='/login' variant='body2'>
                   Already have an account? Login
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-        <Box mt={5} />
-      </Container>
+                    </Link>
+                  </Grid>
+                </Grid>
+              </form>
+            </div>
+            <Box mt={5} />
+          </Container>
+        </FPaper>
+      </Template>
     )
   }
 }
 
-export default SignupComponent
+export default SignUpComponent
